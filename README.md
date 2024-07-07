9a5edfe1b14cfda43d9773809d1a4e749a0c0fd7
Kenneth <kennethsv10@gmail.com>
2024-07-06 22:00:40 -0600

    add logic, fix css

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# encriptador-alura-01
-primer Challenge Programación G7 Alura
-.
+# Encriptador de texto - Challenge 1
+
+En este encriptador creado con JavaScript HTML y CSS, utilizando variables de css, funciones, y demás contenidos aprendidos.
+
+Variables de CSS utilizadas
+
+``` css
+    --primary-color: #0A2463;
+    --secondary-color: #3E92CC;
+    --tertiary-color: #FFFAFF;
+    --background-color: #dff2ff;
+    --active-color: #D8315B;
+    --dark-color: #1E1B18;
+    --gray-color:rgb(39, 37, 37);
+
+    --font-italianno: "Italianno", cursive;
+    --font-roboto-mono: font-family: "Roboto Mono", monospace;
+```
+
+Fuentes
+
+``` css
+@import url('https://fonts.googleapis.com/css2?family=Italianno&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Italianno&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
+```
--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-
-
+let changedVisibility = false;
+const encryptDictionary = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat"
+}
+const decryptDictionary = {
+    ai: "a",
+    enter: "e",
+    imes: "i",
+    ober: "o",
+    ufat: "u"
+}
+function changeVisibility() {
+    if (changedVisibility || (document.querySelector('.encryptor__view__textarea').value == '')) return;
+    document.querySelector('.encryptor__result__container__img').style.display = 'none'
+    document.querySelector('.encryptor__result__container__title').style.display = 'none'
+    document.querySelector('.encryptor__result__container__subtitle').style.display = 'none'
+    document.querySelector('.encryptor__result__container__text').style.display = 'block'
+    document.querySelector('.encryptor__result__container__button').style.display = 'block'
+    document.querySelector('.encryptor__result__container').style.justifyContent = 'space-between'
+    changedVisibility = true;
+}
 
 function encrypt() {
-    const element = document.querySelector('.encryptor__result__container__text').innerText
-    console.log("encrypt");
+    changeVisibility();
+    
+    const text = document.querySelector('.encryptor__view__textarea').value.toLowerCase()
+    let encrypted = text.split(''); // Creamos un Array con todas las letras del texto
+    encrypted = encrypted.map(letter => { // Utilizamos "map" para transformar cada letra en su debido conjunto de "letras encriptadas" 
+        if (encryptDictionary[letter]) {
+            return encryptDictionary[letter]
+        }
+        return letter
+    })
+
+    document.querySelector('.encryptor__result__container__text').innerText = encrypted.join('')
 }
 
 function decrypt() {
-    console.log("decrypt");
+    changeVisibility();
+    const text = document.querySelector('.encryptor__view__textarea').value.toLowerCase()
+    let decrypt = text;
+    for (const key in decryptDictionary) { // For con cada Key del diccionario
+        decrypt = decrypt.split(key) // Creamos un Array separado por la Key que usamos
+        decrypt = decrypt.join(decryptDictionary[key]) // Creamos un nuevo string uniendo todos los elementos del Array y de separador usamos la Key
+        // En síntesis creamos un Array eliminando las "letras encriptadas" y dividiéndolo, donde se encontraban. Luego unimos el Array en un solo string y como separador usamos la letra que correspondía a esas "letras encriptadas"
+    }
+    document.querySelector('.encryptor__result__container__text').innerText = decrypt
 }
 
 function copyResult() {
     const text = document.querySelector('.encryptor__result__container__text').innerText
     navigator.clipboard.writeText(text)
-    console.log(text);
-
 }
